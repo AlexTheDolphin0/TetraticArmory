cfaff7213dd6de9173984bbac26e3557c884ee18
AlexTheDolphim0 <79395212+AlexTheDolphin0@users.noreply.github.com>
2024-08-07 00:46:18 -0500

    Added honing Also fixed my access transformer hating me (just didn't use it)

---
+++ src/main/java/net/alexthedolphin0/tetraticarmory/event/TetraticArmoryEventHandler.java
@@ -0,0 +1,32 @@
+package net.alexthedolphin0.tetraticarmory.event;
+
+import net.alexthedolphin0.tetraticarmory.modular.ItemModularArmor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+@Mod.EventBusSubscriber(modid = "tetratic_armory", bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class TetraticArmoryEventHandler {
+    @SubscribeEvent
+    public static void onLivingEntityHit(LivingAttackEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Iterable<ItemStack> a = Objects.requireNonNull(event.getEntity()).getArmorSlots();
+            a.forEach(stack -> {
+                if (stack.getItem() instanceof ItemModularArmor) {
+                    ((ItemModularArmor)(stack.getItem())).tickHoningProgression(event.getEntity(), stack, 1);
+                    System.out.println("FUCKER");
+                }
+            });
+        }
+    }
+}
--- src/main/java/net/alexthedolphin0/tetraticarmory/mixin/HumanoidArmorLayerMixin.java
+++ src/main/java/net/alexthedolphin0/tetraticarmory/mixin/HumanoidArmorLayerMixin.java
@@ -1,47 +1,31 @@
 package net.alexthedolphin0.tetraticarmory.mixin;
 
 import com.google.common.collect.ImmutableList;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.serialization.DataResult;
+import com.mojang.blaze3d.vertex.*;
 import net.alexthedolphin0.tetraticarmory.modular.ItemModularArmor;
-import net.alexthedolphin0.tetraticarmory.modular.ModularHelmetItem;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.Model;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.model.geom.builders.CubeListBuilder;
-import net.minecraft.client.model.geom.builders.MeshDefinition;
-import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.armortrim.ArmorTrim;
-import org.jline.utils.Colors;
-import org.openjdk.nashorn.internal.parser.JSONParser;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import se.mickelus.tetra.client.model.ColorQuadTransformer;
-import se.mickelus.tetra.items.modular.IModularItem;
 import se.mickelus.tetra.module.data.ModuleModel;
 
-import javax.annotation.Nullable;
-import java.awt.*;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HexFormat;
 
 @Mixin(HumanoidArmorLayer.class)
 public abstract class HumanoidArmorLayerMixin<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>> extends RenderLayer<T, M> {
@@ -61,12 +45,12 @@ public abstract class HumanoidArmorLayerMixin<T extends LivingEntity, M extends 
                 net.minecraft.client.model.Model model = net.minecraftforge.client.ForgeHooksClient.getArmorModel(p_117121_, itemstack, p_117122_, p_117124_);
                 boolean flag = thisnt.usesInnerModel(p_117122_);
                 renderModel(p_117119_, p_117120_, p_117123_, itemstack, model, flag);
-                ArmorTrim.getTrim(p_117121_.level().registryAccess(), itemstack).ifPresent((p_289638_) -> {
+                /*ArmorTrim.getTrim(p_117121_.level().registryAccess(), itemstack).ifPresent((p_289638_) -> {
                     thisnt.renderTrim(armoritem.getMaterial(), p_117119_, p_117120_, p_117123_, p_289638_, model, flag);
-                });
-                if (itemstack.hasFoil()) {
+                });*/
+                /*if (itemstack.hasFoil()) {
                     thisnt.renderGlint(p_117119_, p_117120_, p_117123_, model);
-                }
+                }*/
             }
             ci.cancel();
         }
@@ -77,15 +61,17 @@ public abstract class HumanoidArmorLayerMixin<T extends LivingEntity, M extends 
         ModuleModel[] modelArray = models.toArray(new ModuleModel[0]);
         Arrays.sort(modelArray, Comparator.comparing((ModuleModel o) -> o.renderLayer));
         for (ModuleModel moduleModel : modelArray) {
-            float[] tintRgb = rgb(moduleModel.tint);
+            float[] tintRgb = tetraticArmory$rgb(moduleModel.tint);
             renderModel(poseStack, buffer, i, model, bool, tintRgb[0], tintRgb[1], tintRgb[2], 1.0F, moduleModel.location.withSuffix(".png").withPrefix("textures/model/"));
+            model.renderToBuffer(poseStack, buffer.getBuffer(RenderType.armorEntityGlint()), i, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         }
     }
     public void renderModel(PoseStack p_289664_, MultiBufferSource p_289689_, int p_289681_, Model p_289658_, boolean p_289668_, float red, float green, float blue, float alpha, ResourceLocation armorResource) {
         VertexConsumer vertexconsumer = p_289689_.getBuffer(RenderType.armorCutoutNoCull(armorResource));
         p_289658_.renderToBuffer(p_289664_, vertexconsumer, p_289681_, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0F);
     }
-    private static float[] rgb(int color) {
+    @Unique
+    private static float[] tetraticArmory$rgb(int color) {
         int r = (color >> 16) & 0xFF;
         int g = (color >>  8) & 0xFF;
         int b = (color) & 0xFF;
